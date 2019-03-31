2e911a4b21474d283370becc89e15b3902331c0e
Riccardo <riccardo.buti@gmail.com>
2019-03-31 15:06:26 +0200

    uploaded

---
+++ ServerPorte 2/ConsoleApplication1/Program.cs
@@ -0,0 +1,140 @@
+﻿
+using ConsoleApplication1;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using static System.Net.Mime.MediaTypeNames;
+
+class MyTcpListener
+{
+    public static void Main()
+    {
+        TcpListener server = null;
+        string IpProvenienza = null;
+        DateTime TimeStamp;
+        Uri uri;
+
+        try
+        {
+
+            Utility.WelcomeMesg();
+
+            Int32 port = 8000;
+
+            IPAddress localAddr =  IPAddress.Parse("192.168.1.6"/* Utility.GetLocalIPAddress()*/); //SERVER IP
+
+            
+           // TcpListener server = new TcpListener(port);
+            server = new TcpListener(localAddr, port);
+
+            // Start listening for client requests.
+            server.Start();
+
+            using (StreamWriter w = File.AppendText("log.txt"))
+            {
+                Utility.Log("Inizializzato\t" + DateTime.Now.ToString(), w);
+
+            }
+
+            // Buffer for reading data
+            Byte[] bytes = new Byte[256];
+            String data = null;
+
+
+
+
+            Console.WriteLine("Inizializzato pronto per le connessioni in ingresso.\n\n ");
+            
+            while (true)
+            {
+
+                // Perform a blocking call to accept requests.
+                // You could also user server.AcceptSocket() here.
+                //server.AcceptSocket();
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("\n\nNUOVA CONNESSIONE IN INGRESSO----------------------------------------");
+                Console.WriteLine("\n");
+                data = null;
+                using (StreamWriter w = File.AppendText("log.txt"))
+                {
+                    Utility.Log("Nuova connessione IN\t" + DateTime.Now.ToString(), w);
+
+                }
+                // Get a stream object for reading and writing
+                NetworkStream stream = client.GetStream();
+
+                int i;
+
+                // Loop to receive all the data sent by the client.
+                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    // Translate data bytes to a ASCII string.
+                    //cambiato in codifica utilizzata dai reader
+                    data = Utility.ConvHexDec(System.Text.Encoding.ASCII.GetString(bytes, 0, i));
+                    //data = Convert.ToInt32(System.Text.Encoding.ASCII.GetString(bytes, 0, i), 16).ToString();
+
+                    IpProvenienza = client.Client.RemoteEndPoint.ToString();
+                    TimeStamp = DateTime.Now;
+                    uri = new Uri("http://" + IpProvenienza);
+                    Console.WriteLine("Dato letto: {0} | Inviato da {1} | LogTime: {2}", data, uri.Host.ToString(), TimeStamp.ToString());
+                    using (StreamWriter w = File.AppendText("log.txt"))
+                    {
+                        Utility.Log("Dato letto: "+ data +"\t" + DateTime.Now.ToString(), w);
+
+                    }
+                    new Thread(() =>
+                    {
+                        Thread.CurrentThread.IsBackground = true;
+
+                        if (Utility.CercaDettagliStaff(data) || Utility.CercaDettagliDocente(data) || Utility.CercaDettagliStudente(data))
+                        {
+                            Utility.inviaComando("Ack", uri.Host.ToString());
+                            using (StreamWriter w = File.AppendText("log.txt"))
+                            {
+                                Utility.Log("GRANTED\t" + DateTime.Now.ToString(), w);
+                                Utility.InserisciAccesso(data, Convert.ToInt32(uri.Host.ToString().Substring(11, 2)));
+                            }
+                        }
+                        else
+                        {
+                            //Utility.inviaComando("NAck", uri.Host.ToString());
+                            using (StreamWriter w = File.AppendText("log.txt"))
+                            {
+                                Utility.Log("NOT GRANTED\t" + DateTime.Now.ToString(), w);
+
+                            }
+                        }
+
+                    }).Start();
+
+                }
+
+                // Shutdown and end connection
+                client.Close();
+ 
+            }
+            
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine("Eccezione nel Socket: {0}", e.Message);
+        }
+        finally
+        {
+            // Stop listening for new clients.
+            server.Stop();
+        }
+
+        string myApp = System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase;
+        System.Diagnostics.Process.Start(myApp);
+        Environment.Exit(0);
+  
+    }
+
+
+}
---
+++ ServerPorte 2/ConsoleApplication1/Utility.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Utility
+    {
+
+        public static string  ConvHexDec(string datoLetto)
+        {
+            //FUNZIONE SCRITTA CON IL MIO AMORE EVELYNE PER LA CONVERSIONE DELLE CARD
+            string h2 = datoLetto.Substring(4, 2);
+            string h4 = datoLetto.Substring(2, 2) + datoLetto.Substring(0, 2);
+            string h2Dec = Convert.ToInt32(h2, 16).ToString();
+            string h4Dec = Convert.ToInt32(h4, 16).ToString();
+            return h2Dec.PadLeft(3, '0') + h4Dec.PadLeft(5, '0');
+        }
+
+        internal static void inviaComando(string comando, string Indirizzo)
+        {
+            //CREA LA CONNESSIONE
+            Socket soc = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            System.Net.IPAddress ipAdd = System.Net.IPAddress.Parse(Indirizzo);
+            System.Net.IPEndPoint remoteEP = new IPEndPoint(ipAdd, 8000);
+            soc.Connect(remoteEP);
+
+            //INVIA.
+            byte[] byData = System.Text.Encoding.ASCII.GetBytes(comando);
+            soc.Send(byData);
+            //DISPOSE
+            soc.Close();
+            soc.Dispose();
+        }
+
+        internal static bool CercaDettagliStudente(string data)
+        {
+
+            SqlConnection sqlConnection1 = new SqlConnection("Data Source=my.sienajazz.it;Initial Catalog=SCUOLADB;Persist Security Info=True;User ID=sa;Password=R1cc4rd0");
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader reader;
+
+            cmd.CommandText = "SELECT * FROM Anagrafica where Card = @Card";
+            cmd.Parameters.AddWithValue("@Card", data);
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = sqlConnection1;
+
+            sqlConnection1.Open();
+
+            reader = cmd.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine("TROVATO");
+                    // Process the data sent by the client.
+
+                    Console.WriteLine("Nome Studente: " + reader["Cognome"].ToString() + " " + reader["Nome"].ToString());
+
+                   
+
+                    return true;
+
+                }
+                sqlConnection1.Close();
+
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("NON TROVATO");
+                //inviaComando("bb", IPAddress.Parse(((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString()).ToString());
+
+                return false;
+            }
+
+        }
+
+        internal static void InserisciAccesso(string data, int aula)
+        {
+            SqlConnection sqlConnection1 = new SqlConnection("Data Source=my.sienajazz.it;Initial Catalog=SCUOLADB;Persist Security Info=True;User ID=sa;Password=R1cc4rd0");
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.CommandText = "INSERT INTO Accessi (Nominativo, Aula) VALUES(@Nominativo, @Aula)";
+            cmd.Parameters.AddWithValue("@Nominativo", data);
+            cmd.Parameters.AddWithValue("@Aula", aula);
+
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = sqlConnection1;
+
+            sqlConnection1.Open();
+
+             cmd.ExecuteNonQuery();
+
+            sqlConnection1.Close();
+        }
+
+        internal static bool CercaDettagliStaff(string data)
+        {
+
+            SqlConnection sqlConnection1 = new SqlConnection("Data Source=my.sienajazz.it;Initial Catalog=SCUOLADB;Persist Security Info=True;User ID=sa;Password=R1cc4rd0");
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader reader;
+
+            cmd.CommandText = "SELECT * FROM Staff where Card = @Card";
+            cmd.Parameters.AddWithValue("@Card", data);
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = sqlConnection1;
+
+            sqlConnection1.Open();
+
+            reader = cmd.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine("TROVATO");
+                    // Process the data sent by the client.
+
+                    Console.WriteLine("Nome Staff: " + reader["Cognome"].ToString() + " " + reader["Nome"].ToString());
+
+
+
+                    return true;
+
+                }
+                sqlConnection1.Close();
+
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("NON TROVATO");
+                //inviaComando("bb", IPAddress.Parse(((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString()).ToString());
+
+                return false;
+            }
+
+        }
+
+        internal static bool CercaDettagliDocente(string data)
+        {
+
+            SqlConnection sqlConnection1 = new SqlConnection("Data Source=my.sienajazz.it;Initial Catalog=SCUOLADB;Persist Security Info=True;User ID=sa;Password=R1cc4rd0");
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader reader;
+
+            cmd.CommandText = "SELECT * FROM Docenti where Card = @Card";
+            cmd.Parameters.AddWithValue("@Card", data);
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = sqlConnection1;
+
+            sqlConnection1.Open();
+
+            reader = cmd.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine("TROVATO");
+                    // Process the data sent by the client.
+
+                    Console.WriteLine("Nome Docente: " + reader["Cognome"].ToString() + " " + reader["Nome"].ToString());
+
+
+
+                    return true;
+
+                }
+                sqlConnection1.Close();
+
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("NON TROVATO");
+                //inviaComando("bb", IPAddress.Parse(((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString()).ToString());
+
+                return false;
+            }
+
+        }
+
+        internal static void WelcomeMesg()
+        {
+            Console.WriteLine(@"   _____         _         _  __                                       ");
+            Console.WriteLine(@"  / ____|       | |       | |/ /                                       ");
+            Console.WriteLine(@" | |  __   __ _ | |_  ___ | ' /  ___   ___  _ __    ___  _ __          ");
+            Console.WriteLine(@" | | |_ | / _` || __|/ _ \|  <  / _ \ / _ \| '_ \  / _ \| '__|         ");
+            Console.WriteLine(@" | |__| || (_| || |_|  __/| . \|  __/|  __/| |_) ||  __/| |            ");
+            Console.WriteLine(@"  \_____| \__,_| \__|\___||_|\_\\___| \___|| .__/ _\___||_|     _    _ ");
+            Console.WriteLine(@" |  __ \ (_)                          | |  | |   |  _ \        | |  (_)");
+            Console.WriteLine(@" | |__) | _   ___  ___  __ _  _ __  __| |  |_|   | |_) | _   _ | |_  _ ");
+            Console.WriteLine(@" |  _  / | | / __|/ __|/ _` || '__|/ _` | / _ \  |  _ < | | | || __|| |");
+            Console.WriteLine(@" | | \ \ | || (__| (__| (_| || |  | (_| || (_) | | |_) || |_| || |_ | |");
+            Console.WriteLine(@" |_|  \_\|_| \___|\___|\__,_||_|   \__,_| \___/  |____/  \__,_| \__||_|");
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    Console.WriteLine("SERVER START AT: " + ip.ToString());
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+        public static void Log(string logMessage, TextWriter w)
+        {
+            w.Write("\r\nRegistro di Log : ");
+            w.WriteLine("{0} {1}", DateTime.Now.ToLongTimeString(),
+                DateTime.Now.ToLongDateString());
+            w.WriteLine("  :");
+            w.WriteLine("  :{0}", logMessage);
+            w.WriteLine("-------------------------------");
+        }
+    }
+}
